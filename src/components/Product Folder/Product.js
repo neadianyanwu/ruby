85fb52ae8707ee1123f777fd9e3fde8439dec37d
neadianyanwu <neadianyanwu@gmail.com>
2023-07-19 03:12:31 -0700

    updated commit

--- src/components/Product Folder/Product.js
+++ src/components/Product Folder/Product.js
@@ -2,12 +2,12 @@ import ProductDetails from './ProductDetails';
 import { BsCart3 } from 'react-icons/bs';
 import{ AiOutlineEye, AiOutlineHeart } from 'react-icons/ai';
 import './Product.css';
-import React from 'react'
+import React, { useState } from 'react'
 
 const Product = () => {
+  const [product, setProduct] = useState(ProductDetails)
   return (
     <>
-      
       <div className='products'>
         <h2># Products</h2>
         <p>Home .Products</p>
@@ -16,11 +16,11 @@ const Product = () => {
             <div className='categories'>
               <h3>Categories</h3>
               <ul>
-                <li>iPhones</li>
-                <li>Camera</li>
-                <li>Wireless-Charger</li>
-                <li>Gaming</li>
-                <li>Chandelier-Light</li>
+                <li onClick={() => filterproduct ("iPhones")}>iPhones</li>
+                <li onClick={() => filterproduct ("iPhones")}>Camera</li>
+                <li onClick={() => filterproduct ("iPhones")}>Wireless-Charger</li>
+                <li onClick={() => filterproduct ("iPhones")}>Gaming</li>
+                <li onClick={() => filterproduct ("iPhones")}>Chandelier-Light</li>
                 <li>Smart-Watch</li>
                 <li>Headphone</li>
                 <li>VR-Headset</li>
@@ -34,7 +34,7 @@ const Product = () => {
           <div className='productbox'>
             <div className='content'>
               {
-                ProductDetails.map((curElm) => {
+                product.map((curElm) => {
                   return (
                     <>
                       <div class='box' key={curElm.id}>
